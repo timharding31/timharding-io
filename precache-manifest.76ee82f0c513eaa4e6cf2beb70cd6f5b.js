b3320ffd22cc6086330b827590398d6a544eae2a
timharding <timharding31@gmail.com>
2020-10-02 12:22:53 -0700

    Updates

---
+++ precache-manifest.76ee82f0c513eaa4e6cf2beb70cd6f5b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a06ef198368093bf3c7258e4eb750b70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c48b74439c571e4b17e4",
+    "url": "/static/css/main.716f1108.chunk.css"
+  },
+  {
+    "revision": "db98934c31de1263b49a",
+    "url": "/static/js/2.edbfc060.chunk.js"
+  },
+  {
+    "revision": "2927c17f155caba9c734acdc50e6f829",
+    "url": "/static/js/2.edbfc060.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c48b74439c571e4b17e4",
+    "url": "/static/js/main.95d2f696.chunk.js"
+  },
+  {
+    "revision": "71e302eee25fe37098c2",
+    "url": "/static/js/runtime-main.3bd2fdc9.js"
+  },
+  {
+    "revision": "e8a122bd3a3f60a4217d80486abf4e8c",
+    "url": "/static/media/gg_bridge.e8a122bd.png"
+  },
+  {
+    "revision": "2a84ee73d1a07fd60e25c57375739f3b",
+    "url": "/static/media/icomoon.2a84ee73.woff"
+  },
+  {
+    "revision": "7ff82994ba71bbb254071a718a50d1b8",
+    "url": "/static/media/tim_harding_resume.7ff82994.pdf"
+  },
+  {
+    "revision": "ccba7dc6a6503ac205a12a2c36e05153",
+    "url": "/static/media/uss_constitution.ccba7dc6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/timharding-io/precache-manifest.4ee219d8aa35c63126f6ad53ab9b56fd.js"
+  "/precache-manifest.76ee82f0c513eaa4e6cf2beb70cd6f5b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/timharding-io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
