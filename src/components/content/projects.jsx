eb66c38ae1f293bac22124367612c1ecd1f0270a
timharding <timharding31@gmail.com>
2020-10-06 17:31:28 -0700

    Start on project panes

--- src/components/content/projects.jsx
+++ src/components/content/projects.jsx
@@ -1,11 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Element } from 'react-scroll';
+import technologies from './technologies';
+import projectsList from './projects_list';
 
 export default () => {
+  const renderTech = ({ name, svg }, idx) => (
+    <li key={`tech-${idx}`}>
+      {svg}
+      <p className="tech-name">{name}</p>
+    </li>
+  )
+
+  const changeSelection = key => {
+    return e => {
+      e.preventDefault();
+      setSelected(key);
+    }
+  }
+
+  let [selected, setSelected] = useState('coup');
+
+  const renderTab = project => (
+    <div key={`selector-${project.key}`} className={`project-selector ${selected === project.key ? 'active': ''}`} style={project.tabStyle} onClick={changeSelection(project.key)}>
+      <img src={project.iconSrc} alt=""/>
+      <p style={project.hStyle}>{project.name}</p>
+    </div>
+  )
+
+  const renderProject = (project, state) => {
+    if (project.key === state) {
+      return (
+        <div className="selected-project" key={`selected-${project.key}`} style={project.divStyle}>
+          <div className="project-assets">
+            <div className="project-links">
+              <a style={project.linkStyle} href={project.liveLink} target="_blank">Live Site</a>
+              <a style={project.linkStyle} href={project.sourceLink} target="_blank">Source Code</a>
+            </div>
+            <img className="project-image" src={project.imgSrc} alt={project.key} />
+          </div>
+        <div className="project-description">
+          <p style={project.pStyle}>{project.description}</p>
+        </div>
+        </div>
+      )
+    } else {
+      return null
+    }
+  }
 
   return (
     <Element name={'projects'} title={'My Work'} id='projects'>
-        <h1>My Work</h1>
+      <div className="technologies-pane">
+          <p>I know:</p>
+        <ul className="my-technologies">
+          {technologies.map((tech, idx) => renderTech(tech, idx))}
+        </ul>
+      </div>
+        <div className="my-projects">
+          <p>I've worked on:</p>
+          <div className="project-pane">
+            <div className="project-selectors">
+              {projectsList.map(project => renderTab(project))}
+            </div>
+              {projectsList.map(project => renderProject(project, selected))}
+          </div>
+        </div>
     </Element>
   )
 }
---
+++ src/components/content/projects_list.js
@@ -0,0 +1,111 @@
+import coupFavicon from '../../icons/coup_favicon.png';
+import stockfoxFavicon from '../../icons/stockfox_favicon.png';
+import acrosswordsFavicon from '../../icons/acrosswords_favicon.png';
+import coupImage from '../../images/projects/coup_image.png';
+import stockfoxImage from '../../images/projects/stockfox_image.png';
+import acrosswordsImage from '../../images/projects/acrosswords_image.png';
+
+const coup = {
+  name: 'Coup de Tim',
+  key: 'coup',
+  liveLink: 'https://timharding31.github.io/Coup-de-Tim/',
+  sourceLink: 'https://github.com/timharding31/Coup-de-Tim',
+  technologies: 'Ruby on Rails, PostgreSQL, React, Redux',
+  iconSrc: coupFavicon,
+  imgSrc: coupImage,
+  tabStyle: {
+    backgroundColor: '#FFFDF5',
+    boxShadow: 'inset 0 10px 30px rgba(55, 97, 113, 0.3)',
+  },
+  divStyle: {
+    backgroundColor: '#FFFDF5',
+    boxShadow: 'inset 0 0 10px rgba(55, 97, 113, 0.3)',
+    // boxShadow: 'inset 0 50px 100px #FFFDF5',
+  },
+  linkStyle: {
+    backgroundColor: '#8F3F36',
+    color: '#FFFDF5'
+  },
+  hStyle: {
+    color: '#8F3F36',
+    fontFamily: 'Dancing Script',
+    fontWeight: 700,
+    textShadow: '0 0 1px rgba(22, 28, 29, 0.75)',
+  },
+  pStyle: {
+    color: 'rgba(55, 97, 113, 1)',
+    fontFamily: 'Roboto',
+    fontWeight: 500,
+  },
+  description: 'A web-based version of the popular card game, Coup, built with ‘vanilla’ JavaScript (minimal external libraries).'
+}
+
+const stockFox = {
+  name: 'StockFox',
+  key: 'stockfox',
+  liveLink: 'https://stock-fox.herokuapp.com/',
+  sourceLink: 'https://github.com/timharding31/stock-fox',
+  technologies: 'JavaScript',
+  iconSrc: stockfoxFavicon,
+  imgSrc: stockfoxImage,
+  tabStyle: {
+    backgroundColor: '#1F2124',
+    boxShadow: 'inset 0 10px 30px rgba(255,255,255,0.3)',
+  },
+  divStyle: {
+    backgroundColor: '#1F2124',
+    boxShadow: 'inset 0 0 10px rgba(255,255,255,0.3)',
+    // boxShadow: 'inset 0 50px 100px #1F2124',
+  },
+  linkStyle: {
+    backgroundColor: '#77C151',
+    color: 'rgb(255,255,255)'
+  },
+  hStyle: {
+    color: '#77C151',
+    fontFamily: 'DM Sans',
+    fontWeight: 500,
+  },
+  pStyle: {
+    color: 'rgb(255,255,255)',
+    fontFamily: 'Roboto',
+    fontWeight: 500,
+  },
+  description: 'A stock-trading simulation application inspired by Robinhood.'
+}
+
+const acrossWords = {
+  name: 'AcrossWords',
+  key: 'acrosswords',
+  liveLink: 'https://acrosswords.herokuapp.com/',
+  sourceLink: 'https://github.com/jacobprall/CrossWords',
+  technologies: 'MongoDB, Express, React, Redux, Node, Python, Selenium',
+  iconSrc: acrosswordsFavicon,
+  imgSrc: acrosswordsImage,
+  tabStyle: {
+    backgroundColor: '#F6F5F5',
+    boxShadow: 'inset 0 10px 30px rgba(61, 100, 202, 0.3)',
+  },
+  divStyle: {
+    backgroundColor: '#F6F5F5',
+    boxShadow: 'inset 0 0 10px rgba(61, 100, 202, 0.3)',
+    // boxShadow: 'inset 0 50px 100px #F6F5F5',
+  },
+  linkStyle: {
+    backgroundColor: 'rgb(61, 100, 202)',
+    color: '#F6F5F5'
+  },
+  hStyle: {
+    color: '#404040',
+    fontFamily: 'Orbitron',
+    fontWeight: 700,
+  },
+  pStyle: {
+    color: '#404040',
+    fontFamily: 'Roboto',
+    fontWeight: 500,
+  },
+  description: 'A procedurally generated crossword puzzle that serves up clues dynamically as user input changes.'
+}
+
+export default [coup, stockFox, acrossWords];
