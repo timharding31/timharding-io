b7af988119266657b0f2bba303893d0a643abd5f
timharding <timharding31@gmail.com>
2020-10-06 17:54:51 -0700

    Updates

---
+++ precache-manifest.402e841a08e8495d396fa545fecc62d8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c50491f35d4b4523e86f627c2ef73e9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f10f7bdd41b309d4439",
+    "url": "/static/css/main.6f763c9e.chunk.css"
+  },
+  {
+    "revision": "1f6a1c90714a22f63e42",
+    "url": "/static/js/2.bda0a304.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bda0a304.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f10f7bdd41b309d4439",
+    "url": "/static/js/main.428d2a93.chunk.js"
+  },
+  {
+    "revision": "71e302eee25fe37098c2",
+    "url": "/static/js/runtime-main.3bd2fdc9.js"
+  },
+  {
+    "revision": "c84b39b1b8f2cb08c48188dad9efea0b",
+    "url": "/static/media/TIM_main.c84b39b1.jpg"
+  },
+  {
+    "revision": "492f923d1a6a91d1614079a0c54dca92",
+    "url": "/static/media/TIM_ski.492f923d.jpg"
+  },
+  {
+    "revision": "f69c88f5a605c9423fa52f0828a9044f",
+    "url": "/static/media/acrosswords_image.f69c88f5.png"
+  },
+  {
+    "revision": "15c99c9c898eb578bab4249243991668",
+    "url": "/static/media/coup_favicon.15c99c9c.png"
+  },
+  {
+    "revision": "ea4dc732b6691f9eedc0ff4c90a9bb64",
+    "url": "/static/media/coup_image.ea4dc732.png"
+  },
+  {
+    "revision": "e8a122bd3a3f60a4217d80486abf4e8c",
+    "url": "/static/media/gg_bridge.e8a122bd.png"
+  },
+  {
+    "revision": "1bba59b178ebc1acd72aa1a8f7cfcab7",
+    "url": "/static/media/icomoon.1bba59b1.woff"
+  },
+  {
+    "revision": "a748c17452dedce849f23333a45db7f2",
+    "url": "/static/media/loading.a748c174.gif"
+  },
+  {
+    "revision": "6f796086eee28a1a53572a351781433a",
+    "url": "/static/media/paper_texture.6f796086.png"
+  },
+  {
+    "revision": "649b6815aad0c490423d807e7e7fcca1",
+    "url": "/static/media/stockfox_favicon.649b6815.png"
+  },
+  {
+    "revision": "4c504e7b12a8524442072a61f8c911b5",
+    "url": "/static/media/stockfox_image.4c504e7b.png"
+  },
+  {
+    "revision": "ccba7dc6a6503ac205a12a2c36e05153",
+    "url": "/static/media/uss_constitution.ccba7dc6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8a23f17fabd6a2c06b928d4c2458ca0b.js"
+  "/precache-manifest.402e841a08e8495d396fa545fecc62d8.js"
 );
 
 self.addEventListener('message', (event) => {
